601ecc5100bf733b3fb4d7c7248a3f06401b9b1d
omid <omidbeigi23@gmail.com>
2020-11-27 22:12:11 +0300

    gitignore

--- downloads/management/commands/education.py
+++ downloads/management/commands/education.py
@@ -23,25 +23,23 @@ class Command(BaseCommand):
         log_counter = 0
         for page_number in range(1, 100):
             for url_slug in get_all_udemy_links(page_number=page_number):
-                print(url_slug)
                 single_udemy = get_single_udemy(url_slug)
-                print(single_udemy)
-                # try:
-                # Educational.objects.create(**single_udemy)
-                log_counter += 1
-                # except BaseException as _:
-                #     ex_type, ex_value, ex_traceback = sys.exc_info()
-                #     trace_back = traceback.extract_tb(ex_traceback)
-                #     stack_trace = list()
-                #     for trace in trace_back:
-                #         stack_trace.append(
-                #             "File : %s , Line : %d, Func.Name : %s, Message : %s" % (
-                #                 trace[0], trace[1], trace[2], trace[3]))
-                #     report_log.write("\n Exception type : %s " % ex_type.__name__)
-                #     report_log.write("\n Exception message : %s" % ex_value)
-                #     report_log.write("\n Stack trace : %s" % stack_trace)
-                #     report_log.write("\n URL UDEMY: %s" % single_udemy)
-                #     report_log.write("\n" + "movie count: " + str(log_counter))
-                #     report_log.close()
+                try:
+                    Educational.objects.create(**single_udemy)
+                    log_counter += 1
+                except BaseException as _:
+                    ex_type, ex_value, ex_traceback = sys.exc_info()
+                    trace_back = traceback.extract_tb(ex_traceback)
+                    stack_trace = list()
+                    for trace in trace_back:
+                        stack_trace.append(
+                            "File : %s , Line : %d, Func.Name : %s, Message : %s" % (
+                                trace[0], trace[1], trace[2], trace[3]))
+                    report_log.write("\n Exception type : %s " % ex_type.__name__)
+                    report_log.write("\n Exception message : %s" % ex_value)
+                    report_log.write("\n Stack trace : %s" % stack_trace)
+                    report_log.write("\n URL UDEMY: %s" % single_udemy)
+                    report_log.write("\n" + "movie count: " + str(log_counter))
+                    report_log.close()
         report_log.write("\n" + "movie count: " + str(log_counter))
         report_log.close()
