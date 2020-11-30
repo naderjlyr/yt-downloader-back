13288e9056dc73d2183ac81aed6573f40a35752b
omid <omidbeigi23@gmail.com>
2020-11-30 16:00:03 +0300

    search view added

--- downloads/urls.py
+++ downloads/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
+from downloads import views
 from downloads.view.youtube import youtube_views
 
 urlpatterns = [
     path('download_links', youtube_views.DownloadVideo.as_view({'post': 'generate_youtube_url'})),
     path('youtube_query', youtube_views.DownloadVideo.as_view({'post': 'youtube_query'})),
     path('youtube_auto_suggest', youtube_views.DownloadVideo.as_view({'get': 'youtube_auto_suggest'})),
+    path('search', views.SearchPost.as_view({'get': 'search'})),
 ]
---
+++ downloads/views.py
@@ -0,0 +1,40 @@
+from django.db import models
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from downloads.models import Movie, Adult, Educational
+
+
+class SearchPost(viewsets.ViewSet):
+    @staticmethod
+    def search(request):
+        filters = request.GET
+        search_query = filters['query']
+        filtering_type = filters['filtering_type'].split('-')
+        if 'all' in filtering_type:
+            filtering_type = [
+                'movies',
+                'adult',
+                'educational',
+            ]
+        all_search = {'movies': [], 'adult': [], 'educational': [], 'youtube': []}
+        if 'movies' in filtering_type:
+            movies = Movie.objects.filter(genres__contains=['Action'])
+            # models.Q(name__contains=search_query) | models.Q(description__contains=search_query) | models.Q(
+            #     farsi_name__contains=search_query) |
+            # models.Q(
+            #     genres__in=[["action"]]))
+            all_search['movies'] = movies.values()
+        if 'adult' in filtering_type:
+            adult = Adult.objects.filter(
+                models.Q(name__contains=search_query) | models.Q(description__contains=search_query) | models.Q(
+                    farsi_name__contains=search_query))
+            all_search['adult'] = adult.values()
+
+        if 'educational' in filtering_type:
+            educational = Educational.objects.filter(
+                models.Q(name__contains=search_query) | models.Q(description__contains=search_query) | models.Q(
+                    farsi_name__contains=search_query))
+            all_search['educational'] = educational.values()
+
+        return Response({'status': 'success', 'code': 200, 'data': all_search, 'message': ''})
