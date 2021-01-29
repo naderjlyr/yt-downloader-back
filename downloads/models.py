033a993503296d2b2a58e43d7f3b29843e8f9251
omid <omidbeigi23@gmail.com>
2021-01-28 23:48:07 -0500

    minor changes

--- downloads/models.py
+++ downloads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField, ArrayField
+from django.db.models import JSONField
+from django.contrib.postgres.fields import ArrayField
 from django.utils import timezone
 
 from downloads.choices import MoviesChoices
