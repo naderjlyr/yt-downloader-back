884a62e59a96af80d9649e37ab806ff83ac80892
omid <omidbeigi23@gmail.com>
2021-02-17 18:27:17 -0500

    adult movies updating new ones changed

--- downloads/management/commands/adult.py
+++ downloads/management/commands/adult.py
@@ -15,26 +15,34 @@ def set_sleep(seconds):
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        Adult.objects.all().delete()
         for page_number in range(1, 1000):
             set_sleep(5)
             all_ids = get_all_ids_ujz(page_number=str(page_number))
             for movie_id in all_ids:
                 ujz_single_movie = get_single_movie_ujz(movie_id)
                 try:
-                    Adult.objects.create(**{'name': ujz_single_movie['name'],
-                                            'farsi_name': ujz_single_movie['farsi_name'],
-                                            'description': ujz_single_movie['description'],
-                                            'url': ujz_single_movie['url'],
-                                            'views': ujz_single_movie['views'],
-                                            'rating': ujz_single_movie['rating'],
-                                            'movie_id': ujz_single_movie['movie_id'],
-                                            'tags': ujz_single_movie['tags'],
-                                            'image': ujz_single_movie['image'],
-                                            'download_links': [ujz_single_movie['download_links']],
-                                            }
+                    # using url because movie_id is not reliable (just a random slug)
+                    existing_video = Adult.objects.filter(url__exact=ujz_single_movie['url'])
+                    if existing_video.count() > 0:
+                        existing_video.update(
+                            views=ujz_single_movie['views'],
+                            rating=ujz_single_movie['rating'],
+                            download_links=[ujz_single_movie['download_links']],
+                        )
+                    else:
+                        Adult.objects.create(**{'name': ujz_single_movie['name'],
+                                                'farsi_name': ujz_single_movie['farsi_name'],
+                                                'description': ujz_single_movie['description'],
+                                                'url': ujz_single_movie['url'],
+                                                'views': ujz_single_movie['views'],
+                                                'rating': ujz_single_movie['rating'],
+                                                'movie_id': ujz_single_movie['movie_id'],
+                                                'tags': ujz_single_movie['tags'],
+                                                'image': ujz_single_movie['image'],
+                                                'download_links': [ujz_single_movie['download_links']],
+                                                }
 
-                                         )
+                                             )
 
 
                 except BaseException as _:
